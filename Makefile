bba95a61f3c80817e094084821301f2c2505d964
Morten Kristensen <msk@nullpointer.dk>
2018-07-17 19:49:29 +0200

    Test seed accum and next

---
+++ Makefile
@@ -0,0 +1,9 @@
+CXX=c++
+
+all: test
+
+clean:
+	rm -fv tests
+
+test:
+	${CXX} -Ofast -std=c++11 -DNO_EOSIO tests.cc -o tests && ./tests
---
+++ tests.cc
@@ -0,0 +1,82 @@
+#include "Random.h"
+using namespace eosblox;
+
+#include <cassert>
+#include <string>
+#include <vector>
+
+static inline void assertSeedEq(const Random &gen, const uint64_t seed)
+{
+  assert(gen.seed() == seed);
+}
+
+static inline void assertNextEq(Random &gen, const uint64_t value)
+{
+  assert(gen.next() == value);
+}
+
+void testSeedAccumulation()
+{
+  Random gen(1);
+  assertSeedEq(gen, 1);
+
+  // Integer.
+  gen.accumSeed(1);
+  assertSeedEq(gen, 11295943761408656531LLU);
+
+  // Initializer list.
+  gen.accumSeedRange({1, 2, 3});
+  assertSeedEq(gen, 18083823154816788002LLU);
+
+  // General ranges.
+  gen.accumSeedRange(std::string("Hello, World!"));
+  assertSeedEq(gen, 7880214006551886600LLU);
+
+  const std::string str("Hello, World!");
+  gen.accumSeedRange(str);
+  assertSeedEq(gen, 11498166657420254716LLU);
+
+  gen.accumSeedRange(std::vector<int>{1, 2, 3});
+  assertSeedEq(gen, 4175012130207295590LLU);
+
+  // Arrays.
+  const int arr[3]{1, 2, 3};
+  gen.accumSeedArray(arr);
+  assertSeedEq(gen, 4326221722869819695LLU);
+}
+
+void testNext()
+{
+  {
+    Random gen(1);
+    assertNextEq(gen, 17243114145050246623LLU);
+    assertNextEq(gen, 5516405404208600209LLU);
+    assertNextEq(gen, 4897608421861629041LLU);
+    assertNextEq(gen, 8669024630748878125LLU);
+    assertNextEq(gen, 13562114715725036114LLU);
+  }
+  {
+    Random gen(111970);
+    assertNextEq(gen, 8987405948927848382LLU);
+    assertNextEq(gen, 4621043010224079033LLU);
+    assertNextEq(gen, 15658672667220659145LLU);
+    assertNextEq(gen, 14814678247671488241LLU);
+    assertNextEq(gen, 849456367364724273LLU);
+  }
+  {
+    Random gen(1);
+    gen.accumSeedRange(std::string("Hello, World!"));
+    assertNextEq(gen, 14607242223934345243LLU);
+    assertNextEq(gen, 12308690566605765243LLU);
+    assertNextEq(gen, 6585550730876598598LLU);
+    assertNextEq(gen, 5507355692446769180LLU);
+    assertNextEq(gen, 16383900445848122088LLU);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  testSeedAccumulation();
+  testNext();
+  return 0;
+}
